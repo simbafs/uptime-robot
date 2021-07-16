a2d2f58d3c9bc5151ba1e5355da03b6046fc00ee
simba-fs <simba.fs@gmail.com>
2021-07-16 09:30:21 +0800

    finish command /server listen and /server list

--- src/parseCmd.js
+++ src/parseCmd.js
@@ -1,25 +1,64 @@
 const Discord = require('discord.js');
+const { join } = require('path');
+const low = require('lowdb');
+const FileSync = require('lowdb/adapters/FileSync');
+
+const db = low(new FileSync(join(process.cwd(), 'db.json')));
 
 function getArgs(cmd){
 	cmd = cmd.options[0].options;
 	let args = {};
-	cmd.forEach(i => args[i.name] = i.value);
+	cmd?.forEach(i => args[i.name] = i.value);
 	return args;
 }
 
-const subCmdListen = (cmd, send) => {
+const subCmdListen = (cmd, send, interaction) => {
 	const args = getArgs(cmd);
-	console.log('listen sub-command', args);
+	let { url, channel } = args;
+
+	if(!channel) channel = interaction.channel_id;
+
+	try{
+		let c = db.get(['channel', channel]);
+		if(c.value()){
+			if(c.value().includes(url)){
+				send(`${url} is allready in listening list`);
+			}else{
+				c.push(url).write();
+				send(`add ${url} to channel ${channel}'s listening list`);
+			}
+		}else{
+			db.set(['channel', channel], [url])
+				.write();
+			send(`add ${url} to channel ${channel}'s listening list`);
+		}
+	}catch(e){
+		send('Error occurred');
+		console.error(e);
+	}
 }
 
-const subCmdRemove = (cmd, send) => {
+const subCmdRemove = (cmd, send, interaction) => {
 	const args = getArgs(cmd);
-	console.log('listen sub-command', args);
+	send(`
+\`\`\`json
+${JSON.stringify(cmd, null, 2)}
+\`\`\`
+	`);
+	console.log('remove sub-command', args);
 }
 
-const subCmdList = (cmd, send) => {
+const subCmdList = (cmd, send, interaction) => {
 	const args = getArgs(cmd);
-	console.log('listen sub-command', args);
+	let { channel } = args;
+
+	if(!channel) channel = interaction.channel_id;
+
+	let c = db
+		.get(['channel', channel])
+		.value();
+	send(`\`\`\`json
+${JSON.stringify(c, null, 2)}\`\`\``);
 }
 
 const cmds = {
@@ -29,23 +68,29 @@ const cmds = {
 }
 
 function Parse(interaction, client){
-	const send = (content) => client.api.interactions(interaction.id, interaction.token).callback.post({
-		data: {
-			type: 4,
-			data: { content }
-		}
-	});
+	let flag = false
+	const send = (content) => {
+		console.log('send', flag, content);
+		if(flag) return;
+		client.api.interactions(interaction.id, interaction.token)
+			.callback.post({
+				data: {
+					type: 4,
+					data: { content }
+				}
+			});
+		flag = true;
+	};
 	const cmd = interaction.data;
 	const subCmd = cmd.options[0].name;
-	if(subCmd in cmds){
-		cmds[subCmd](cmd, send);
-	}
-
-	send(`
-\`\`\`json
+	if(cmds[subCmd]){
+		console.log('exec', cmds[subCmd]);
+		cmds[subCmd](cmd, send, interaction);
+	}else{
+		send(`\`\`\`json
 ${JSON.stringify(cmd, null, 2)}
-\`\`\`
-	`);
+\`\`\``);
+	}
 }
 
 module.exports = Parse;
---
+++ src/registerCmd.js
@@ -0,0 +1,97 @@
+const config = require('config');
+const {
+	DiscordInteractions,
+	ApplicationCommandOptionType: OptionType
+} = require('slash-commands');
+
+// register commands
+const interaction = new DiscordInteractions({
+	applicationId: config.get('appID'),
+	authToken: config.get('token'),
+	publicKey: config.get('publicKey'),
+});
+
+// delete command
+// const delCmd = ['865065460683505685', '865110899685720065', '865112530368069652'];
+// delCmd.forEach(i => interaction.deleteApplicationCommand(i, null).then(console.log, console.error));
+
+// list commands
+interaction
+	.getApplicationCommands()
+	.then(console.log)
+	.catch(console.error);
+
+// const cmdPing = {
+//     name: "ping",
+//     description: "ping server",
+//     options: [
+//         {
+//             name: "n",
+//             description: "times to ping",
+//             type: OptionType.INTEGER
+//         },
+//     ],
+// };
+
+const cmdServer = {
+	name: 'server',
+	description: 'manage channels in the list',
+	options: [
+		{
+			name: 'listen',
+			description: 'Add url to the channel listening list',
+			type: OptionType.SUB_COMMAND,
+			options: [
+				{
+					name: 'url',
+					description: 'url to monitor',
+					required: true,
+					type: OptionType.STRING
+				},
+				{
+					name: 'channel',
+					description: 'On which channel the message will be displayed',
+					required: false,
+					type: OptionType.CHANNEL
+				}
+			]
+		},
+		{
+			name: 'remove',
+			description: 'Remove url from channel listening list',
+			type: OptionType.SUB_COMMAND,
+			options: [
+				{
+					name: 'url',
+					description: 'url to monitor',
+					required: true,
+					type: OptionType.STRING
+				},
+				{
+					name: 'channel',
+					description: 'On which channel the message will be displayed',
+					required: false,
+					type: OptionType.CHANNEL
+				}
+			]
+		},
+		{
+			name: 'list',
+			description: 'Show channel listening list',
+			type: OptionType.SUB_COMMAND,
+			options: [
+				{
+					name: 'channel',
+					description: 'On which channel the message will be displayed',
+					required: false,
+					type: OptionType.CHANNEL
+				}
+			]
+		}
+	]
+};
+
+interaction
+	.createApplicationCommand(cmdServer, config.get('guildID'))
+	.then(console.log)
+	.catch(console.error);
